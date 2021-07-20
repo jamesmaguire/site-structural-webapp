a6f67f656854dbf94e987b42cda0c7a7975ea97e
James Maguire <james.maguire@indesco.com.au>
2021-07-20 14:02:31 +1000

    Refactor rc-column and update drawFigure function

--- static/rc-column.js
+++ static/rc-column.js
@@ -1,44 +1,118 @@
 
 function updatePage()
 {
-    calcLoads();
-    calcBarParams();
-    drawFigure(0.3);
+    // calcLoads();
+    // calcBarParams();
+    drawFigure();
     setStatusUptodate();
 }
 
-function drawFigure(scale)
-{
+function drawFigure() {
     let canvas = document.getElementById('columnFigure');
-    if (canvas.getContext)
-    {
-        let ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let ctx = canvas.getContext('2d');
+    let X = canvas.width;
+    let Y = canvas.height;
+    ctx.clearRect(0, 0, X, Y);
 
-        // Variables
-        let Xc = canvas.width/2;
-        let Yc = canvas.height/2;
-        let Dx = colDx.valueAsNumber;
-        let Dy = colDy.valueAsNumber;
-        let c = cover.valueAsNumber;
-        let dt = tiedb.valueAsNumber;
-        let db = bardb.valueAsNumber;
-        let barsTB = nbarsTB.valueAsNumber;
-        let barsS = nbarsSide.valueAsNumber;
+    let col = {
+        L:i_L.valueAsNumber,
+        Dx:i_Dx.valueAsNumber,
+        Dy:i_Dy.valueAsNumber,
+        Dia:i_Dia.valueAsNumber,
+        c:i_c.valueAsNumber,
+        db:i_db.valueAsNumber,
+        barspcx:i_barspcx.valueAsNumber,
+        barspcy:i_barspcy.valueAsNumber,
+        dbt:i_dbt.valueAsNumber,
+    };
+    let scalefactor = 0.9*Math.min(canvas.width/col.Dx, canvas.height/col.Dy);
+    if (i_shape.value == "circ") {
+        scalefactor = 0.8*Math.min(canvas.width/col.Dia, canvas.height/col.Dia);
+    }
+    for (key in col) {
+        col[key] = scalefactor*col[key];
+    }
+    col.nbarstop = i_nbarstop.valueAsNumber;
+    col.nbarsside = i_nbarsside.valueAsNumber;
+    col.nbarscirc = i_nbarscirc.valueAsNumber;
+    col.nbars = o_nbars.valueAsNumber;
+    col.shape = i_shape.value;
 
-        // Concrete mass
-        ctx.lineWidth = 1;
-        ctx.fillStyle = 'lightgray';
+    if (col.shape == "rect") {
+        // Column outline
         ctx.beginPath();
-        ctx.rect(Xc-scale*(Dx/2),Yc-scale*(Dy/2),scale*Dx,scale*Dy);
-        ctx.fill();
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        ctx.rect((X-col.Dx)/2,(Y-col.Dy)/2,col.Dx,col.Dy);
         ctx.stroke();
 
-        // Tie
-        
+        // Top bars
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        let barspc = (col.Dx-2*(col.c+col.dbt)-col.db)/(col.nbarstop-1);
+        console.log(barspc);
+        for (i = 0; i < col.nbarstop; i++) {
+            rebar(ctx,
+                  (X-col.Dx)/2 + col.c+col.dbt+col.db/2 + (i*barspc),
+                  (Y-col.Dy)/2 + (col.c+col.dbt+col.db/2),
+                  col.db);
+            rebar(ctx,
+                  (X-col.Dx)/2 + col.c+col.dbt+col.db/2 + (i*barspc),
+                  (Y+col.Dy)/2 - (col.c+col.dbt+col.db/2),
+                  col.db);
+        }
 
-        // Long bars
-        
+        // Side bars
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        barspc = (col.Dy-2*(col.c+col.dbt)-col.db)/(col.nbarsside-1);
+        for (i = 1; i < col.nbarsside-1; i++) {
+            rebar(ctx,
+                  (X-col.Dx)/2 + (col.c+col.dbt+col.db/2),
+                  (Y-col.Dy)/2 + col.c+col.dbt+col.db/2 + (i*barspc),
+                  col.db);
+            rebar(ctx,
+                  (X+col.Dx)/2 - (col.c+col.dbt+col.db/2),
+                  (Y-col.Dy)/2 + col.c+col.dbt+col.db/2 + (i*barspc),
+                  col.db);
+        }
+
+        // Stirrups
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        // I've trimmed one pixel to give the stirrup a more visually consistent size
+        roundedrect(ctx, (X-col.Dx)/2+col.c+1, (Y-col.Dy)/2+col.c+1,
+                    col.Dx-2*col.c-2, col.Dy-2*col.c-2, 3*col.dbt/2);
+        roundedrect(ctx, (X-col.Dx)/2+col.c+col.dbt, (Y-col.Dy)/2+col.c+col.dbt,
+                    col.Dx-2*col.c-2*col.dbt, col.Dy-2*col.c-2*col.dbt, col.dbt/2);
+
+    } else if (col.shape == 'circ') {
+        // Column outline
+        ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        ctx.arc(X/2 ,Y/2 , col.Dia/2, 0, 2*Math.PI);
+        ctx.stroke();
+
+        // Stirrups
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.arc(X/2 ,Y/2 , col.Dia/2-col.c-1, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(X/2 ,Y/2 , col.Dia/2-col.c-col.dbt, 0, 2*Math.PI);
+        ctx.stroke();
+
+        // Longitudinal bars
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        for (angle = 0; angle < 2*Math.PI; angle+=(2*Math.PI)/col.nbarscirc) {
+            rebar(ctx,
+                  X/2 + (col.Dia/2-col.c-col.dbt-col.db/2)*Math.cos(angle),
+                  Y/2 + (col.Dia/2-col.c-col.dbt-col.db/2)*Math.sin(angle), col.db);
+        }
+
     }
 }
 
@@ -46,7 +120,20 @@ function rebar(ctx, x, y, db)
 {
     ctx.beginPath();
     ctx.arc(x, y, db/2, 0, 2*Math.PI, false);
-    ctx.fill();
+    ctx.stroke();
+}
+
+function roundedrect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
     ctx.stroke();
 }
 
