d4c2bf06de8bb713f6e7c0e369e9050d1b97c560
James Maguire <3rdmaguire@gmail.com>
2021-08-22 10:20:35 +1000

    Add rebar rates

--- static/rebar-properties.js
+++ static/rebar-properties.js
@@ -7,41 +7,52 @@ function updatePage()
 
 function fillOutputs()
 {
-    area.value = calcArea().toPrecision(3);
-    aream.value = (calcArea()/1000**2).toExponential(2);
-    secondmoment.value = calcSecondMoment().toPrecision(3);
-    secondmomentm.value = (calcSecondMoment()/1000**4).toPrecision(3);
-    sectionmodulus.value = calcSectionModulus().toPrecision(3);
-    sectionmodulusm.value = (calcSectionModulus()/1000**3).toPrecision(3);
+    // Inputs
+    let db = i_db.valueAsNumber;
+    let spc = i_spc.valueAsNumber;
+    let fsy = i_fsy.valueAsNumber;
+    let rho = i_rho.valueAsNumber;
 
-    Ncapacity.value = calcAxialCapacity().toPrecision(3);
-    Mcapacity.value = calcMomentCapacity().toPrecision(3);
+    // Cross section props
+    area.value = calcArea(db).toPrecision(3);
+    aream.value = (calcArea(db)/1000**2).toExponential(2);
+    secondmoment.value = calcSecondMoment(db).toPrecision(3);
+    secondmomentm.value = (calcSecondMoment(db)/1000**4).toPrecision(3);
+    sectionmodulus.value = calcSectionModulus(db).toPrecision(3);
+    sectionmodulusm.value = (calcSectionModulus(db)/1000**3).toPrecision(3);
+
+    // Rates
+    o_barid.value = "N"+i_db.valueAsNumber;
+    o_barmass.value = (calcArea(db)*rho/1000**2).toPrecision(3);
+    o_arearate.value = (calcArea(db)*1000/spc).toFixed();
+    o_barrate.value = (rho*calcArea(db)*1000/spc/1000**2).toPrecision(3);
+    o_ratetwoway.value = (2*rho*calcArea(db)*1000/spc/1000**2).toPrecision(3);
+    o_rateef.value = (2*rho*calcArea(db)*1000/spc/1000**2).toPrecision(3);
+    o_ratetwowayef.value = (4*rho*calcArea(db)*1000/spc/1000**2).toPrecision(3);
+
+    // Capacity
+    Mcapacity.value = calcMomentCapacity(fsy, db).toPrecision(3);
+    Ncapacity.value = calcAxialCapacity(fsy, db).toPrecision(3);
 }
 
-function calcArea()
+function calcArea(db)
 {
-    let db = diameter.valueAsNumber;
     return Math.PI*db**2/4;
 }
 
-function calcSectionModulus() {
-    let db = diameter.valueAsNumber;
-    return calcSecondMoment()/(db/2);
+function calcSectionModulus(db) {
+    return calcSecondMoment(db)/(db/2);
 }
 
-function calcSecondMoment()
+function calcSecondMoment(db)
 {
-    let db = diameter.valueAsNumber;
     return Math.PI*db**4/64;
 }
 
-function calcAxialCapacity() {
-    let fsy = yieldStrength.valueAsNumber;
-    return fsy*calcArea()/1000; // kN
+function calcAxialCapacity(fsy, db) {
+    return fsy*calcArea(db)/1000; // kN
 }
 
-function calcMomentCapacity() {
-    // let db = diameter.valueAsNumber;
-    let fsy = yieldStrength.valueAsNumber;
-    return fsy*calcSectionModulus()/1000**2; // kNm
+function calcMomentCapacity(fsy, db) {
+    return fsy*calcSectionModulus(db)/1000**2; // kNm
 }
