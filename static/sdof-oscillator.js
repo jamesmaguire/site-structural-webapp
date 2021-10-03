8b5074b6e63d5a98399eb6b0dcfc4141ce57dc01
James Maguire <3rdmaguire@gmail.com>
2021-10-03 15:28:27 +1100

    Update sdof svg plots

--- static/sdof-oscillator.js
+++ static/sdof-oscillator.js
@@ -20,6 +20,8 @@ function initPage()
     // Analysis
     input('i_dt', {initval: 0.1});
     input('i_duration', {initval: 10});
+    output('o_umin');
+    output('o_umax');
 
     updatePage();
 }
@@ -33,74 +35,55 @@ function updatePage()
 
 function plotForce()
 {
-    let dt = i_dt.valueAsNumber;
-    let duration = i_duration.valueAsNumber;
-    let nsteps = Math.floor(duration/dt, 1);
-    let t = [...Array(nsteps).keys()].map(i => dt*i);
-    let fpeak = i_fpeak.valueAsNumber;
-    let td = i_td.valueAsNumber;
-    let f = t.map(ti => fTriangularImpulse(ti, fpeak, td));
-
-    let svgNS = 'http://www.w3.org/2000/svg';
-    svg = document.createElementNS(svgNS, 'svg');
-    svg.setAttributeNS(null, 'width', 500);
-    svg.setAttributeNS(null, 'height', 500);
-    svg.setAttributeNS(null, 'viewBox', `${-0.1*duration} ${-0.1*fpeak} ${1.1*duration} ${1.1*fpeak}`);
-    svg.setAttributeNS(null, 'preserveAspectRatio',"xMidYMid");
-
-    let linepath = `M${t[0]},${-f[0]}`;
-    for (i=0; i<t.length; i++) {
-        linepath += ` L${t[i]},${-f[i]}`;
-    }
-    let line = document.createElementNS(svgNS, 'path');
-    line.setAttributeNS(null, 'd', linepath);
-    line.setAttributeNS(null, 'fill', 'none');
-    line.setAttributeNS(null, 'stroke', 'black');
-    line.setAttributeNS(null, 'stroke-width', .1);
-
     document.getElementById('forcePlot').innerHTML='';
-    document.getElementById('forcePlot').appendChild(svg).appendChild(line);
-}
+    const svgNS = 'http://www.w3.org/2000/svg';
 
-function plotResponse(x, y)
-{
-    let Width = 500;
-    let Height = 500;
-    let margin = {left:50, right:50, top:50, bottom:50};
-    let plotH = Height - margin.top - margin.bottom;
-    let plotW = Width - margin.left - margin.right;
+    const dt = i_dt.valueAsNumber,
+          duration = i_duration.valueAsNumber,
+          nsteps = Math.floor(duration/dt, 1),
+          t = [...Array(nsteps).keys()].map(i => dt*i),
+          fpeak = i_fpeak.valueAsNumber,
+          td = i_td.valueAsNumber,
+          f = t.map(ti => fTriangularImpulse(ti, fpeak, td));
 
-    let xmin = x.reduce((a, b) => a < b ? a : b);
-    let xmax = x.reduce((a, b) => a > b ? a : b);
-    let ymin = y.reduce((a, b) => a < b ? a : b);
-    let ymax = y.reduce((a, b) => a > b ? a : b);
-    let dx = xmax - xmin;
-    let dy = ymax - ymin;
-    x = x.map(n => n*plotW/dx + margin.left + plotW*xmin/dx);
-    y = y.map(n => n*plotH/dy - margin.top - plotH*ymax/dy);
+    const margin = {left:50, right:50, top:50, bottom:50},
+          height = 500 - margin.top - margin.bottom,
+          width = 500 - margin.left - margin.right;
 
-    let svgNS = 'http://www.w3.org/2000/svg';
-    svg = document.createElementNS(svgNS, 'svg');
-    svg.setAttributeNS(null, 'width', Width);
-    svg.setAttributeNS(null, 'height', Height);
-    svg.setAttributeNS(null, 'viewBox', `0 0 ${Width} ${Height}`);
+    const xmin = t.reduce((a, b) => a < b ? a : b),
+          xmax = t.reduce((a, b) => a > b ? a : b),
+          ymin = f.reduce((a, b) => a < b ? a : b)-0.5*Math.abs(fpeak),
+          ymax = f.reduce((a, b) => a > b ? a : b)+0.5*Math.abs(fpeak),
+          dx = xmax - xmin,
+          dy = ymax - ymin;
+
+    const xmap = n => (n+xmin)*width/dx + margin.left;
+    const ymap = n => (-n+ymax)*height/dy + margin.top;
+
+    const svg = document.createElementNS(svgNS, 'svg');
+    svg.setAttributeNS(null, 'width', width + margin.left + margin.right);
+    svg.setAttributeNS(null, 'height', height + margin.top + margin.bottom);
+    svg.setAttributeNS(null, 'viewBox', `0 0 `
+                       +`${width + margin.left + margin.top} `
+                       +`${height + margin.top + margin.bottom}`);
     svg.setAttributeNS(null, 'preserveAspectRatio',"xMidYMid");
+    document.getElementById('forcePlot').appendChild(svg);
 
     let xaxis = document.createElementNS(svgNS, 'line');
     xaxis.setAttributeNS(null, 'class', 'axis');
-    xaxis.setAttributeNS(null, 'x1', margin.left);
-    xaxis.setAttributeNS(null, 'x2', Width - margin.right);
-    xaxis.setAttributeNS(null, 'y1', Height-margin.bottom+ymin/dy*plotH);
-    xaxis.setAttributeNS(null, 'y2', Height-margin.bottom+ymin/dy*plotH);
+    xaxis.setAttributeNS(null, 'x1', xmap(xmin));
+    xaxis.setAttributeNS(null, 'x2', xmap(xmax));
+    xaxis.setAttributeNS(null, 'y1', ymap(0));
+    xaxis.setAttributeNS(null, 'y2', ymap(0));
     svg.appendChild(xaxis);
 
     let xticks = document.createElementNS(svgNS, 'g');
     xticks.setAttributeNS(null, 'class', 'xticks');
-    [{text:xmin, loc:margin.left}, {text:xmax, loc:Width-margin.right}]
+    [{text:xmin, loc:xmap(xmin)}, {text:xmax, loc:xmap(xmax)}]
         .map(function(t) {
             let tick = document.createElementNS(svgNS, 'text');
             tick.setAttributeNS(null, 'x', t.loc);
-            tick.setAttributeNS(null, 'y', Height-margin.bottom+ymin/dy*plotH);
+            tick.setAttributeNS(null, 'y', ymap(0));
             tick.innerHTML = t.text;
             xticks.appendChild(tick);
         });
@@ -108,18 +91,108 @@ function plotResponse(x, y)
 
     let yaxis = document.createElementNS(svgNS, 'line');
     yaxis.setAttributeNS(null, 'class', 'axis');
-    yaxis.setAttributeNS(null, 'x1', margin.left);
-    yaxis.setAttributeNS(null, 'x2', margin.left);
-    yaxis.setAttributeNS(null, 'y1', Height - margin.bottom);
-    yaxis.setAttributeNS(null, 'y2', margin.top);
+    yaxis.setAttributeNS(null, 'x1', xmap(0));
+    yaxis.setAttributeNS(null, 'x2', xmap(0));
+    yaxis.setAttributeNS(null, 'y1', ymap(ymin));
+    yaxis.setAttributeNS(null, 'y2', ymap(ymax));
     svg.appendChild(yaxis);
 
     let yticks = document.createElementNS(svgNS, 'g');
     yticks.setAttributeNS(null, 'class', 'yticks');
-    [{text:ymin, loc:Height-margin.bottom}, {text:ymax, loc:margin.top}]
+    let tick = document.createElementNS(svgNS, 'text');
+    tick.setAttributeNS(null, 'x', xmap(0));
+    tick.setAttributeNS(null, 'y', ymap(fpeak));
+    tick.innerHTML = fpeak.toPrecision(2);
+    yticks.appendChild(tick);
+    svg.appendChild(yticks);
+
+    let xlabel = document.createElementNS(svgNS, 'text');
+    xlabel.setAttributeNS(null, 'class', 'xlabel');
+    xlabel.setAttributeNS(null, 'x', xmap((xmin+xmax)/2));
+    xlabel.setAttributeNS(null, 'y', ymap(0));
+    xlabel.innerHTML = 't';
+    svg.appendChild(xlabel);
+
+    let ylabel = document.createElementNS(svgNS, 'text');
+    ylabel.setAttributeNS(null, 'class', 'ylabel');
+    ylabel.setAttributeNS(null, 'x', xmap(0));
+    ylabel.setAttributeNS(null, 'y', ymap(ymax));
+    ylabel.innerHTML = 'f(t)';
+    svg.appendChild(ylabel);
+
+    let linepath = `M${xmap(t[0])},${ymap(f[0])}`;
+    for (i=0; i<t.length; i++) {
+        linepath += ` L${xmap(t[i])},${ymap(f[i])}`;
+    }
+    let line = document.createElementNS(svgNS, 'path');
+    line.setAttributeNS(null, 'class', 'plotline');
+    line.setAttributeNS(null, 'd', linepath);
+    svg.appendChild(line);
+
+}
+
+function plotResponse(x, y)
+{
+    document.getElementById('responsePlot').innerHTML='';
+    const svgNS = 'http://www.w3.org/2000/svg';
+
+    const margin = {left:50, right:50, top:50, bottom:50},
+          height = 500 - margin.top - margin.bottom,
+          width = 500 - margin.left - margin.right;
+
+    const xmin = x.reduce((a, b) => a < b ? a : b),
+          xmax = x.reduce((a, b) => a > b ? a : b),
+          ymin = y.reduce((a, b) => a < b ? a : b),
+          ymax = y.reduce((a, b) => a > b ? a : b),
+          dx = xmax - xmin,
+          dy = ymax - ymin;
+
+    const xmap = n => (n+xmin)*width/dx + margin.left;
+    const ymap = n => (-n+ymax)*height/dy + margin.top;
+
+    const svg = document.createElementNS(svgNS, 'svg');
+    svg.setAttributeNS(null, 'width', width + margin.left + margin.right);
+    svg.setAttributeNS(null, 'height', height + margin.top + margin.bottom);
+    svg.setAttributeNS(null, 'viewBox', `0 0 `
+                       +`${width + margin.left + margin.top} `
+                       +`${height + margin.top + margin.bottom}`);
+    svg.setAttributeNS(null, 'preserveAspectRatio',"xMidYMid");
+    document.getElementById('responsePlot').appendChild(svg);
+
+    let xaxis = document.createElementNS(svgNS, 'line');
+    xaxis.setAttributeNS(null, 'class', 'axis');
+    xaxis.setAttributeNS(null, 'x1', xmap(xmin));
+    xaxis.setAttributeNS(null, 'x2', xmap(xmax));
+    xaxis.setAttributeNS(null, 'y1', ymap(0));
+    xaxis.setAttributeNS(null, 'y2', ymap(0));
+    svg.appendChild(xaxis);
+
+    let xticks = document.createElementNS(svgNS, 'g');
+    xticks.setAttributeNS(null, 'class', 'xticks');
+    [{text:xmin, loc:xmap(xmin)}, {text:xmax, loc:xmap(xmax)}]
         .map(function(t) {
             let tick = document.createElementNS(svgNS, 'text');
-            tick.setAttributeNS(null, 'x', margin.left);
+            tick.setAttributeNS(null, 'x', t.loc);
+            tick.setAttributeNS(null, 'y', ymap(0));
+            tick.innerHTML = t.text;
+            xticks.appendChild(tick);
+        });
+    svg.appendChild(xticks);
+
+    let yaxis = document.createElementNS(svgNS, 'line');
+    yaxis.setAttributeNS(null, 'class', 'axis');
+    yaxis.setAttributeNS(null, 'x1', xmap(0));
+    yaxis.setAttributeNS(null, 'x2', xmap(0));
+    yaxis.setAttributeNS(null, 'y1', ymap(ymin));
+    yaxis.setAttributeNS(null, 'y2', ymap(ymax));
+    svg.appendChild(yaxis);
+
+    let yticks = document.createElementNS(svgNS, 'g');
+    yticks.setAttributeNS(null, 'class', 'yticks');
+    [{text:ymin, loc:ymap(ymin)}, {text:ymax, loc:ymap(ymax)}]
+        .map(function(t) {
+            let tick = document.createElementNS(svgNS, 'text');
+            tick.setAttributeNS(null, 'x', xmap(xmin));
             tick.setAttributeNS(null, 'y', t.loc);
             tick.innerHTML = t.text.toPrecision(2);
             yticks.appendChild(tick);
@@ -128,30 +201,27 @@ function plotResponse(x, y)
 
     let xlabel = document.createElementNS(svgNS, 'text');
     xlabel.setAttributeNS(null, 'class', 'xlabel');
-    xlabel.setAttributeNS(null, 'x', margin.left + plotW/2);
-    xlabel.setAttributeNS(null, 'y', Height-margin.bottom+ymin/dy*plotH);
+    xlabel.setAttributeNS(null, 'x', xmap((xmin+xmax)/2));
+    xlabel.setAttributeNS(null, 'y', ymap(0));
     xlabel.innerHTML = 't';
     svg.appendChild(xlabel);
 
     let ylabel = document.createElementNS(svgNS, 'text');
     ylabel.setAttributeNS(null, 'class', 'ylabel');
-    ylabel.setAttributeNS(null, 'x', margin.left);
-    ylabel.setAttributeNS(null, 'y', margin.top + plotH/2);
+    ylabel.setAttributeNS(null, 'x', xmap(0));
+    ylabel.setAttributeNS(null, 'y', ymap(0));
     ylabel.innerHTML = 'u(t)';
     svg.appendChild(ylabel);
 
-    let linepath = ` M${x[0]},${-y[0]}`;
+    let linepath = ` M${xmap(x[0])},${ymap(y[0])}`;
     for (i=1; i<x.length; i++) {
-        linepath += ` L${x[i]},${-y[i]}`;
+        linepath += ` L${xmap(x[i])},${ymap(y[i])}`;
     }
     let line = document.createElementNS(svgNS, 'path');
     line.setAttributeNS(null, 'class', 'plotline');
     line.setAttributeNS(null, 'd', linepath);
-    line.setAttributeNS(null, 'fill', 'none');
     svg.appendChild(line);
 
-    document.getElementById('responsePlot').innerHTML='';
-    document.getElementById('responsePlot').appendChild(svg);
 }
 
 function runCalcs()
@@ -193,6 +263,7 @@ function runCalcs()
         }
         u[i+1] = phati/khat;
     }
+
     // let v = [v0];
     // let a = [a0];
     // for (i=1; i<nsteps; i++) {
@@ -200,6 +271,8 @@ function runCalcs()
     //     a.push( (u[i+1] - 2*u[i] - u[i-1])/dt**2 );
     // }
     
+    o_umin.value = u.reduce((a, b) => a < b ? a : b).toPrecision(3);
+    o_umax.value = u.reduce((a, b) => a > b ? a : b).toPrecision(3);
     plotResponse(t, u);
 
 }
